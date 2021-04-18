f537adc8771c1c2a178564464ac94d5b4a0195e3
thiegomoura <thi-ego@hotmail.com>
2021-04-18 18:21:51 -0400

    configure avatar upload

--- src/models/User.ts
+++ src/models/User.ts
@@ -20,6 +20,9 @@ class User {
   @Column()
   password: string;
 
+  @Column()
+  avatar_url: string;
+
   @CreateDateColumn()
   created_at: Date;
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticaded';
 
@@ -34,7 +35,17 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.status(220).send();
-})
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  })
 
 export default usersRouter;
