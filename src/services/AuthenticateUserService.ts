a570b78bab6954f1cdff464ee3a9f230a5b4246f
thiegomoura <thi-ego@hotmail.com>
2021-04-18 14:38:46 -0400

    finish authenticate route

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -26,12 +28,18 @@ class AuthenticateUserService {
 
     const passwordMatched = await compare(password, user.password);
 
-    if (!passwordMatched){
+    if (!passwordMatched) {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, '91ee0f1a607999e27547e4e966b7da96', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
